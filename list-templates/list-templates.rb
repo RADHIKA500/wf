7776d6a3fdf4b9972569c49e83dbe4d400228340
RADHIKA500 <30533892+RADHIKA500@users.noreply.github.com>
2024-07-22 16:37:34 +0530

    Add files via upload

---
+++ list-templates/list-templates.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+require "manageiq-api-client"
+
+# Image-specific parameters
+ems_id = ENV.fetch("PROVIDER_ID")
+
+# ManageIQ API login
+secrets = JSON.load(File.read(ENV.fetch("_CREDENTIALS")))
+secrets.transform_keys! { |k| k.sub(/^api_/, "").to_sym }
+
+url        = ENV.fetch("API_URL", "http://localhost:3000")
+verify_ssl = ENV.fetch("VERIFY_SSL", "true") == "true"
+
+api_options = {url: url, ssl: {verify: verify_ssl}}.merge(secrets)
+api = ManageIQ::API::Client.new(api_options)
+
+# Get the list of templates
+resources = api.templates.where(:ems_id => ems_id).pluck(:ems_ref, :name)
+
+# Output the list of templates in JSON format to STDOUT
+puts({"values" => resources.to_h}.to_json)
