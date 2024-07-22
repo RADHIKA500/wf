76d7a0d2d9a6a4e3673d2f14eb62618a2791f676
RADHIKA500 <30533892+RADHIKA500@users.noreply.github.com>
2024-07-22 16:36:31 +0530

    Add files via upload

---
+++ list-providers/list-providers.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+require "json"
+require "manageiq-api-client"
+
+# Image-specific parameters
+provider_type = ENV.fetch("PROVIDER_TYPE", nil)
+
+# ManageIQ API login
+secrets = JSON.load(File.read(ENV.fetch("_CREDENTIALS")))
+secrets.transform_keys! { |k| k.sub(/^api_/, "").to_sym }
+
+url        = ENV.fetch("API_URL", "http://localhost:3000")
+verify_ssl = ENV.fetch("VERIFY_SSL", "true") == "true"
+
+api_options = {url: url, ssl: {verify: verify_ssl}}.merge(secrets)
+api = ManageIQ::API::Client.new(api_options)
+
+# Get the list of providers
+response = api.providers
+response = response.where(:type => provider_type) if provider_type
+response = response.pluck(:id, :name)
+
+# Output the list of providers in JSON format to STDOUT
+puts({"values" => response.to_h}.to_json)
